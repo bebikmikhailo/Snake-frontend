fef78a95a32b79f2d106b860109724ec5cde9fdd
John Pork <mbebyk@outlook.com>
2026-05-15 19:33:10 +0300

    sadfasdfsd

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    API_BASE_URL: "http://snake-production-3297.up.railway.app",
+    API_BASE_URL: "https://snake-production-3297.up.railway.app",
     SERVER_PORT: "3000",
     DEFAULT_AVATAR_PATH: "./assets/images/default-avatar.webp"
 };
